63ec8c9aa5c075bf290ae17179015579e10968ee
Mattis <mattis.babel@edu.ece.fr>
2019-12-06 20:30:56 +0100

    Main Page basics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import FormBecomeNoteTaker from './Components/FormBecomeNoteTaker'
+import FormINeedNoteTaker from './Components/FormINeedNoteTaker'
 
 class App extends Component {
 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            printFormBecomeNoteTaker: false,
+            printFormINeedNoteTaker: false,
+        }
+
+    }
+
     handleINeedNoteTaker = () => {
-        console.log("J'ai besoin d'un preneur de note")
+      this.setState({printFormINeedNoteTaker: true})
+      this.setState({printFormBecomeNoteTaker: false})
     }
 
     handleBecomeNoteTaker = () => {
-        console.log("Je veux devenir un preneur de note")
+        this.setState({printFormBecomeNoteTaker: true})
+        this.setState({printFormINeedNoteTaker: false})
     }
 
-    render() 
+    render()
     {
         return(
             <div style={styles.main_container}>
@@ -18,20 +32,20 @@ class App extends Component {
                     <button style={styles.connexion_button}>Connexion</button>
                 </div>
                 <div style={styles.inscription}>
-                    <div 
+                    <div
                         style={styles.I_need_note_taker}
                         onClick={this.handleINeedNoteTaker}
                         >
+                        <FormINeedNoteTaker printFormINeedNoteTaker = {this.state.printFormINeedNoteTaker}/>
                     </div>
-                    <div 
+                    <div
                         style={styles.become_note_taker}
                         onClick={this.handleBecomeNoteTaker}
                         >
+                        <FormBecomeNoteTaker printFormBecomeNoteTaker = {this.state.printFormBecomeNoteTaker}/>
                     </div>
+
                 </div>
-                <p style = {{position: "fixed", top: "60%", left: "25%",  transform: "translate(-50%, -50%)"}}>J'ai besoin d'un preneur de note</p>
-                <p style = {{position: "fixed", top: "60%", left: "75%",  transform: "translate(-50%, -50%)"}}>Je veux devenir preneur de note</p>
-                <p style = {{position: "fixed", top: "50%", left: "50%",  transform: "translate(-50%, -50%)"}}>⬅️ Inscription ➡️</p>
             </div>
         );
     }
@@ -45,20 +59,22 @@ const styles = {
         height: "90vh"
     },
     nav: {
-        backgroundColor: 'yellow',
+        backgroundColor: 'red',
         width: '100%',
         height: '10vh   '
     },
     inscription: {
-        display: 'flex'
+        display: 'flex',
+        backgroundImage: "url(" + "https://imagesia.com/images/2019/12/06/background2.png" + ")",
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
     },
     become_note_taker: {
-        backgroundColor: 'green',
         width: '50%',
         height: '90vh'
     },
     I_need_note_taker: {
-        backgroundColor: 'purple',
         width: '50%',
         height: '90vh'
     },
@@ -66,7 +82,7 @@ const styles = {
         backgroundColor: 'red',
         border: '2px solid black',
         borderRadius: '10px',
-        position: 'fixed', 
+        position: 'fixed',
         right: '0%',
         top: '5%',
         transform: "translate(-50%, -50%)"
---
+++ src/Components/FormINeedNoteTaker.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+
+export default class FormINeedNoteTaker extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            need_nom: "",
+            need_prenom: "",
+            need_ufr: "",
+            need_year: "",
+            need_cm: "",
+            need_dispo: ""
+        }
+
+    }
+
+    printFormINeedNoteTaker = () => {
+        if (this.props.printFormINeedNoteTaker === true)
+        {
+            return(
+            <div style={styles.formulaire}>
+                <form onSubmit={this.handleSubmit}>
+                    <label style={styles.legend}><span style={styles.number}>1</span> Identité</label>
+                    <br/>
+                    <input type="text" placeholder="Nom" style={styles.textArea} value={this.state.need_nom} onChange = {(event) => {this.setState({need_nom: event.target.value})}}/>
+                    <br/>
+                    <input type="text" placeholder="Prénom" style={styles.textArea} value={this.state.need_prenom} onChange = {(event) => {this.setState({need_prenom: event.target.value})}}/>
+                    <br/>
+                    <br/>
+                    <label style={styles.legend}><span style={styles.number}>2</span> Informations Etudiantes</label>
+                    <br/>
+                    <input type="text" placeholder="UFR" style={styles.textArea} value={this.state.need_ufr} onChange = {(event) => {this.setState({need_ufr: event.target.value})}}/>
+                    <br/>
+                    <input type="text" placeholder="Année" style={styles.textArea} value={this.state.need_year} onChange = {(event) => {this.setState({need_year: event.target.value})}}/>
+                    <br/>
+                    <input type="text" placeholder="CM" style={styles.textArea} value={this.state.need_cm} onChange = {(event) => {this.setState({need_cm: event.target.value})}}/>
+                    <br/>
+                    <br/>
+                    <label style={styles.legend}><span style={styles.number}>3</span> Disponibilités</label>
+                    <br/>
+                    <textarea type="text" placeholder="Disponibilités" style={styles.textArea} value={this.state.need_dispo} onChange = {(event) => {this.setState({need_dispo: event.target.value})}}/>
+                    <br/><br/><br/>
+                     <input type="submit" value="Envoyer" />
+              </form>
+          </div>)
+      }else{
+          return(<div></div>)
+      }
+    }
+
+    render()
+    {
+        return(
+            <this.printFormINeedNoteTaker/>
+        );
+    }
+}
+
+const styles = {
+
+    formulaire: {
+        width: '20%',
+        height: '80vh',
+        left: '15%',
+        top: '15%',
+        position: 'fixed',
+        zIndex: 2,
+    	padding: 20,
+    	backgroundColor: '#f4f7f8',
+    	margin: 10,
+    	backgroundColor: '#f4f7f8',
+    	borderRadius: 8,
+    	fontFamily: "Georgia"
+    },
+
+    number:{
+        background: '#1abc9c',
+    	color: '#fff',
+    	height: 30,
+    	width: 30,
+    	display: 'inline-block',
+    	fontSize: 20,
+    	lineHeight: 1.3,
+    	textAlign: 'center',
+    	textShadow: 'rgba(255,255,255,0.2)',
+    	borderRadius: 15,
+    },
+
+    legend:{
+        fontSize: 20,
+    },
+
+    textArea: {
+        fontFamily: "Georgia",
+    	background: "rgba(255,255,255,.1)",
+    	border: "none",
+    	borderRadius: 4,
+    	fontSize: 15,
+    	margin: 0,
+    	outline: 0,
+    	padding: 10,
+    	width: '100%',
+    	boxSizing: 'border-box',
+    	webkitBoxSizing: 'border-box',
+    	mozBoxSizing: 'border-box',
+    	backgroundColor: '#e8eeef',
+    	color: '#8a97a0',
+    	webkitBoxShadow: "rgba(0,0,0,0.03)",
+    	boxShadow: "rgba(0,0,0,0.03)",
+    	marginBottom: 30}
+    }
