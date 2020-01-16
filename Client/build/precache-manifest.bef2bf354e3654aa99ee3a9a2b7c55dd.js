ee4a8d2ae0ec86b41250b2f6b41b02f66f81f363
ThomLlobEce <thomas.llobregat@edu.ece.fr>
2020-01-16 12:13:03 +0100

    build added to git

---
+++ Client/build/precache-manifest.bef2bf354e3654aa99ee3a9a2b7c55dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0584b545c8cc4cef8b899dea592ceb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2d334c5b159ab600b59",
+    "url": "/static/js/2.2f5fa6c0.chunk.js"
+  },
+  {
+    "revision": "a3940f15a98ad68b68ed",
+    "url": "/static/js/main.da5c242a.chunk.js"
+  },
+  {
+    "revision": "14834abdd642f50c0207",
+    "url": "/static/js/runtime-main.0a0af7ea.js"
+  },
+  {
+    "revision": "4b6179cb8d8d3654295c04316e17dc12",
+    "url": "/static/media/Rima.4b6179cb.png"
+  },
+  {
+    "revision": "c332df24813107addc2041f903010100",
+    "url": "/static/media/logo.c332df24.png"
+  }
+]);
